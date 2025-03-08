e42c9fb2b59cab690b34dc92fd209adfa39326c5
HeikaHaku <admin@omnious.org>
2025-03-08 03:21:16 -0600

    Set up subgroups for Nav Drawer.

--- components/Header.vue
+++ components/Header.vue
@@ -18,24 +18,57 @@
   </v-app-bar>
 
   <v-navigation-drawer v-model="drawer" temporary>
-    <v-list>
-      <v-list-item
-        :value="item"
-        v-for="(item, i) in items"
-        :key="i"
-        :to="item.to"
-        :disabled="item.to === route.path"
-      >
-        <template v-slot:prepend>
-          <v-icon :icon="item.icon" />
-        </template>
-        <v-list-item-title v-text="item.title" />
-      </v-list-item>
+    <v-list v-model:opened="opened">
+      <template v-for="(item, i) in items" :key="i">
+        <v-list-item
+          v-if="!item.children"
+          :value="item"
+          :to="item.to"
+          :disabled="item.to === route.path"
+          :prepend-icon="item.icon"
+          :title="item.title?.toString()"
+        />
+
+        <v-list-group v-else :value="item.title?.toString()">
+          <template #activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              :value="item"
+              prepend-icon="mdi-view-list"
+              :title="item.title?.toString()"
+            />
+          </template>
+
+          <v-list-item
+            :value="item"
+            :to="item.to"
+            :disabled="item.to === route.path"
+            :prepend-icon="item.icon"
+            :title="item.title?.toString()"
+          />
+
+          <v-list-item
+            v-for="(child, j) in item.children"
+            :key="j"
+            :prepend-icon="child.icon"
+            :title="child.title?.toString()"
+            :disabled="child.to === route.path"
+            :to="child.to"
+            :value="child"
+          />
+        </v-list-group>
+      </template>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts" setup>
+  import type {
+    RouteRecordNameGeneric,
+    RouteRecordNormalized,
+    RouteRecordRaw,
+  } from 'vue-router';
+
   const drawer = ref(false);
 
   const router = useRouter();
@@ -54,11 +87,36 @@
 
   const routes = router.getRoutes();
 
-  const items = routes.map((route) => ({
+  interface NavItem {
+    title: RouteRecordNameGeneric;
+    to: string;
+    icon: string;
+    children?: NavItem[];
+  }
+
+  const createNav = (route: RouteRecordNormalized): NavItem => ({
     title: route.name,
     to: route.path,
-    icon: routeIcons[route.name?.toString().toLowerCase() || '404'],
-  }));
+    icon:
+      routeIcons[route.name?.toString().toLowerCase() || '404'] ||
+      routeIcons['404'],
+    ...(route.children.length > 0
+      ? {
+          children: route.children.map((child) =>
+            createNav(getChildRouteData(child)),
+          ),
+        }
+      : {}),
+  });
+
+  const getChildRouteData = (route: RouteRecordRaw): RouteRecordNormalized =>
+    routes.find((r) => r.name === route.name)!; // We know it should exist, so we can assert that. Linter can fuck off.
+
+  const items = routes
+    .filter((route) => !route.path.includes('/', 1))
+    .map(createNav);
+
+  const opened = ref([] as string[]);
 
   // const items = [
   //   {
