631433fecfcbaa01919d88c8a5c72bdbf6f453fb
hexhivemind <hexhivemind@proton.me>
2025-03-08 18:20:21 +0900

    feat: added basic upload field and button skeleton

--- components/Home.vue
+++ components/Home.vue
@@ -1,4 +1,39 @@
 <template>
-  <span>Test</span>
-  <v-btn>test</v-btn>
+  <v-row>
+    <v-col>
+      <v-file-input
+        label="Upload Files"
+        prepend-icon="mdi-paperclip"
+        :rules="[filesRequired]"
+        v-model="files"
+        multiple
+        chip
+        show-size
+      />
+    </v-col>
+
+    <v-col>
+      <v-btn prepend-icon="mdi-cloud-upload" @click="handleUpload">
+        Upload
+      </v-btn>
+    </v-col>
+  </v-row>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        files: [],
+        filesRequired: (value) =>
+          value.length > 0 || 'You must select at least one file.',
+      };
+    },
+    methods: {
+      handleUpload() {
+        // Handle file upload action here
+        console.log('Upload button clicked', this.files);
+      },
+    },
+  };
+</script>
