af04ad32e5df704f0d1538e2360adc770b2d1656
hexhivemind <hexhivemind@proton.me>
2025-03-08 16:20:35 +0900

    feat: grabbed basic appbar off the shelf from vuetify3

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar
+    :elevation="1"
+    rounded
+    scroll-behavior="collapse fade-image"
+    scroll-threshold="200"
+  >
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>HexHive</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn icon="mdi-magnify"></v-btn>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
