de2919f6eb5945c4a6541b0980519104d30456f8
HeikaHaku <admin@omnious.org>
2025-03-08 00:39:13 -0600

    Remove Nuxt seo all in one.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'vuetify-nuxt-module', '@nuxtjs/seo'],
+  modules: [
+    '@nuxt/eslint',
+    'vuetify-nuxt-module',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
+  ],
   typescript: {
     typeCheck: true,
   },
