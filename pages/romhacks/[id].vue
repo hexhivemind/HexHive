6d91bfec36d0a6fcfc0a10dd73a857f3538823db
hexhivemind <hexhivemind@proton.me>
2025-03-09 17:13:40 +0900

    feat: add basic layout skeleton for a minimal listing

--- pages/romhacks/[id].vue
+++ pages/romhacks/[id].vue
@@ -1,5 +1,14 @@
 <template>
-  <span />
+  <Listing :data="data" />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const data: ListingData = {
+    id: 'FEEDB0B0DEADBEEF',
+    title: 'Fire Red Omega++',
+    description: "A continuation hack of Drayano's Fire Red Omega",
+    author: 'Jordank',
+    // slug: '',
+    // rating: ''
+  };
+</script>
