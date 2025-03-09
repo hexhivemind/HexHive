d75ea8969fc79518c0d3cd06e83c0f1c36db6f37
hexhivemind <hexhivemind@proton.me>
2025-03-09 17:36:06 +0900

    fix: skeleton contents did not get pushed with last commit

--- pages/romhacks/[id].vue
+++ pages/romhacks/[id].vue
@@ -3,11 +3,22 @@
 </template>
 
 <script lang="ts" setup>
-  const data: ListingData = {
-    id: 'FEEDB0B0DEADBEEF',
+  const route = useRoute();
+
+  const data: RomhackData = {
+    id: route.params.id as string,
     title: 'Fire Red Omega++',
     description: "A continuation hack of Drayano's Fire Red Omega",
     author: 'Jordank',
+    filename: 'FRO++.gba',
+    release: 'v4.9',
+    releaseDate: 'December 2024',
+
+    // 1636 Pokemon Fire Red (U)(Squirrels)
+    baseRom: 'Fire Red',
+    baseRomVersion: 'v1.0',
+    baseRomRegion: 'English',
+
     // slug: '',
     // rating: ''
   };
