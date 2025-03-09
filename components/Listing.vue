6fe37744a6ca1d1068762f354027d9fcfb8a1c6e
hexhivemind <hexhivemind@proton.me>
2025-03-09 17:35:21 +0900

    feat: fleshing out romhack listing skeleton

--- components/Listing.vue
+++ components/Listing.vue
@@ -1,9 +1,21 @@
 <template>
   <v-container fluid>
     <v-card>
-      <h1>{{ model.title }}</h1>
+      <h1>{{ model.title }} - {{ model.release }}</h1>
       <h2>{{ model.description }}</h2>
       <h3>By: {{ model.author }}</h3>
+
+      <br />
+
+      <p>
+        Based on: {{ model.baseRom }} Version &mdash;
+        {{ model.baseRomVersion }} ({{ model.baseRomRegion }})
+      </p>
+
+      <p>{{ model.release }} Release Date: {{ model.releaseDate }}</p>
+
+      <br />
+      <small>ID or Slug: {{ model.id }}</small>
     </v-card>
 
     <!--
@@ -25,5 +37,6 @@
     name: 'Listing',
   });
 
-  const model = defineModel<ListingData>('data', { required: true });
+  // For testing, change data type, should be ListingData eventually
+  const model = defineModel<RomhackData>('data', { required: true });
 </script>
