392c9e670931083172b108afcff1ad48ca12cdb3
HeikaHaku <admin@omnious.org>
2025-03-28 07:26:46 -0500

    fix: Fix type error with deleted entries.

--- components/Listings.vue
+++ components/Listings.vue
@@ -19,8 +19,14 @@
       </template>
       <template v-else>
         <v-col v-for="(n, i) in data" :key="i" cols="4">
+          <template v-if="!isDeleted(n)">
           <!-- TODO: Add artwork to cards based on type and data -->
           <v-card height="200" :to="`${route.path}/${n.id}`" />
+          </template>
+          <template v-else>
+            <!-- TODO: Decide how to properly display deleted entries -->
+            <v-card height="200" :to="`${route.path}/${n._id}`" />
+          </template>
         </v-col>
       </template>
     </v-row>
@@ -35,6 +41,10 @@
   const route = useRoute();
 
   const data = defineModel<ListEntry<ListingData>[]>('data', { default: [] });
+
+  function isDeleted(entry: ListEntry<ListingData>): entry is DeletedEntry {
+    return (entry as DeletedEntry).deleted === true;
+  }
 
   const { breadcrumbs } = useBreadcrumbs();
 </script>
