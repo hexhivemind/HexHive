40993722aed9f124f9fd35a8a97f0a88762c53a7
HeikaHaku <admin@omnious.org>
2025-04-04 02:14:11 -0500

    feat: DRY Romhacks Schema.

--- server/models/Listings/Romhacks.ts
+++ server/models/Listings/Romhacks.ts
@@ -1,15 +1,30 @@
-import type { Model } from 'mongoose';
-import mongoose, { Schema, model } from 'mongoose';
+import mongoose, { Schema, model, type Model } from 'mongoose';
 
 import { BaseListings } from './Shared';
+import { runtimeTypes } from '~/types/runtimeTypes.generated';
 
 const RomhacksSchema = new Schema<RomhackData>({
   ...BaseListings,
 
   filename: { type: String, required: true },
-  baseRom: { type: String, required: true, enum: [], index: true },
-  baseRomVersion: { type: String, required: true, enum: [], index: true },
-  baseRomRegion: { type: String, required: true, enum: [], index: true },
+  baseRom: {
+    type: String,
+    required: true,
+    enum: runtimeTypes.SupportedBaseRom,
+    index: true,
+  },
+  baseRomVersion: {
+    type: String,
+    required: true,
+    enum: runtimeTypes.SupportedBaseRomVersion,
+    index: true,
+  },
+  baseRomRegion: {
+    type: String,
+    required: true,
+    enum: runtimeTypes.SupportedBaseRomRegion,
+    index: true,
+  },
   release: { type: String, required: true },
   lastUpdated: { type: Date, default: Date.now() },
 
