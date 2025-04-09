744e163446761b328dbc0b31386bdd56dbec0b32
HeikaHaku <admin@omnious.org>
2025-04-09 01:01:53 -0500

    refactor: Romhack endpoints.

--- server/api/romhacks/index.get.ts
+++ server/api/romhacks/index.get.ts
@@ -2,6 +2,9 @@ import Romhacks from '~/server/models/Listings/Romhacks';
 
 export default defineEventHandler(async () => {
   await connectMongoose();
-  const romhacks = await Romhacks.find().lean<RomhackData[]>().exec();
+  const romhacks = await Romhacks.find()
+    .sort({ releaseDate: -1 })
+    .lean<RomhackData[]>()
+    .exec();
   return romhacks;
 });
