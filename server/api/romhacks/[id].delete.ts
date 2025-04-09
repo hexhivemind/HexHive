dac089909845fb7de6b2db633173b4f6c29c7406
HeikaHaku <admin@omnious.org>
2025-04-09 17:52:53 -0500

    feat: Allow delete by slug, id, or _id.

--- server/api/romhacks/[id].delete.ts
+++ server/api/romhacks/[id].delete.ts
@@ -1,8 +1,18 @@
+import { isValidObjectId, type FilterQuery } from 'mongoose';
 import Romhacks from '~/server/models/Listings/Romhacks';
 
 export default defineEventHandler(async (event) => {
   await connectMongoose();
   const id = event.context.params?.id;
-  await Romhacks.findByIdAndDelete(id).exec();
+
+  // TODO: Archival soft deletion
+  await Romhacks.findOneAndDelete({
+    $or: [
+      { slug: id },
+      !isNaN(Number(id)) ? { id: Number(id) } : null,
+      isValidObjectId(id) ? { _id: id } : null,
+    ].filter(Boolean) as FilterQuery<RomhackData>[],
+  }).exec();
+
   return { success: true };
 });
