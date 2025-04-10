6e23cfc13304ede394d25bd53018ac0625a5a5f0
HeikaHaku <admin@omnious.org>
2025-04-09 22:56:16 -0500

    feat: Sprite endpoints.

---
+++ server/api/sprites/[id].patch.ts
@@ -0,0 +1,12 @@
+import Sprites from '~/server/models/Listings/Sprites';
+import { SpriteDataSchema } from '~/shared/zod';
+
+export default defineEventHandler(async (event) => {
+  return await createListingEndpoint(
+    event,
+    Sprites,
+    'updateListing',
+    'Sprite',
+    SpriteDataSchema,
+  );
+});
---
+++ server/api/sprites/index.get.ts
@@ -0,0 +1,5 @@
+import Sprites from '~/server/models/Listings/Sprites';
+
+export default defineEventHandler(async (event) => {
+  return await createListingEndpoint(event, Sprites, 'fetchAll', 'Sprite');
+});
