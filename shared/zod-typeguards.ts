86fecebc2008af08ca09e7e919090ebc3a20a444
HeikaHaku <admin@omnious.org>
2025-04-16 09:24:57 -0500

    refactor: More efficient typing on type-guards

--- shared/zod-typeguards.ts
+++ shared/zod-typeguards.ts
@@ -3,12 +3,8 @@ import { SpriteCategorySchema } from './zod-helpers';
 
 export const isValidCategory = (
   data: unknown,
-): data is
-  | SpriteUnionType
-  | SpriteUnionType[]
-  | Record<string, SpriteUnionType> => {
-  return SpriteCategorySchema.safeParse(data).success;
-};
+): data is SpritesData['category'] =>
+  SpriteCategorySchema.safeParse(data).success;
 
 export const isValidSpritesData = (data: unknown): data is SpritesData =>
   SpriteDataSchema.safeParse(data).success;
