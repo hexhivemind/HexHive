c5eb4cce8756874f2da979c0a46d7633ed2b6742
HeikaHaku <admin@omnious.org>
2025-05-18 15:01:42 -0500

    style: Further fixes for layout

--- pages/profile/[username].vue
+++ pages/profile/[username].vue
@@ -1,110 +1,100 @@
 <template>
-  <v-container dense class="h-[90%]">
+  <v-container dense class="md:h-9/10 h-full">
     <v-row class="h-full gap-6" dense no-gutters>
       <!-- Sidebar -->
-      <v-col cols="12" md="4" class="h-full">
-        <v-row class="h-full gap-6" dense no-gutters>
+      <v-col cols="12" md="4" class="md:h-full">
+        <div class="h-full flex flex-col gap-6">
           <!-- Nametag -->
-          <v-col cols="12">
-            <v-card class="px-5 py-5 text-center relative h-full">
-              <span
-                v-if="user.role !== 'user'"
-                :class="[
-                  'absolute top-3 right-3 px-3 py-1 rounded-full text-xs capitalize ',
-                  roleColor[user.role],
-                ]"
-              >
-                {{ user.role }}
-              </span>
+          <v-card class="px-5 py-5 text-center relative">
+            <span
+              v-if="user.role !== 'user'"
+              :class="[
+                'absolute top-3 right-3 px-3 py-1 rounded-full text-xs capitalize ',
+                roleColor[user.role],
+              ]"
+            >
+              {{ user.role }}
+            </span>
 
-              <div class="flex flex-col items-center">
-                <v-img
-                  class="rounded-full"
-                  width="128"
-                  height="128"
-                  cover
-                  src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-                />
-                <!-- :src="user.avatar" -->
+            <div class="flex flex-col items-center">
+              <v-img
+                class="rounded-full"
+                width="128"
+                height="128"
+                cover
+                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+              />
+              <!-- :src="user.avatar" -->
 
-                <div class="flex items-center">
-                  <h2 class="text-xl font-semibold">
-                    {{ user.displayName || user.username }}
-                  </h2>
-                  <p class="text-xs ml-2">({{ user.pronouns }})</p>
-                </div>
-
-                <p>@{{ user.username }}</p>
+              <div class="flex items-center">
+                <h2 class="text-xl font-semibold">
+                  {{ user.displayName || user.username }}
+                </h2>
+                <p class="text-xs ml-2">({{ user.pronouns }})</p>
               </div>
-            </v-card>
-          </v-col>
+
+              <p>@{{ user.username }}</p>
+            </div>
+          </v-card>
 
           <!-- STATS ROW -->
-          <v-col cols="12">
-            <v-card class="px-5 py-5 h-full">
-              <h3 class="font-semibold text-lg">📊 User Stats</h3>
-              <ul class="space-y-2">
-                <li>
-                  <strong>Joined on: </strong>
-                  <v-tooltip bottom>
-                    <template #default>
-                      <span>
-                        {{ joinDate.toFormat('yyyy-LL-dd HH:mm:ss') }}
-                      </span>
-                    </template>
-                    <template #activator="{ props }">
-                      <span v-bind="props">
-                        {{ joinDate.toFormat('yyyy-LL-dd') }}
-                      </span>
-                    </template>
-                  </v-tooltip>
-                </li>
-                <li>
-                  <strong>Last Seen: </strong>
-                  <v-tooltip bottom>
-                    <template #default>
-                      <span>
-                        {{ lastActiveDate.toFormat('yyyy-LL-dd HH:mm:ss') }}
-                      </span>
-                    </template>
-                    <template #activator="{ props }">
-                      <span v-bind="props">
-                        {{ lastActiveDate.toFormat('yyyy-LL-dd') }}
-                      </span>
-                    </template>
-                  </v-tooltip>
-                </li>
-              </ul>
-            </v-card>
-          </v-col>
+          <v-card class="px-5 py-5 flex-grow">
+            <h3 class="font-semibold text-lg">📊 User Stats</h3>
+            <ul class="space-y-2">
+              <li>
+                <strong>Joined on: </strong>
+                <v-tooltip bottom>
+                  <template #default>
+                    <span>
+                      {{ joinDate.toFormat('yyyy-LL-dd HH:mm:ss') }}
+                    </span>
+                  </template>
+                  <template #activator="{ props }">
+                    <span v-bind="props">
+                      {{ joinDate.toFormat('yyyy-LL-dd') }}
+                    </span>
+                  </template>
+                </v-tooltip>
+              </li>
+              <li>
+                <strong>Last Seen: </strong>
+                <v-tooltip bottom>
+                  <template #default>
+                    <span>
+                      {{ lastActiveDate.toFormat('yyyy-LL-dd HH:mm:ss') }}
+                    </span>
+                  </template>
+                  <template #activator="{ props }">
+                    <span v-bind="props">
+                      {{ lastActiveDate.toFormat('yyyy-LL-dd') }}
+                    </span>
+                  </template>
+                </v-tooltip>
+              </li>
+            </ul>
+          </v-card>
 
           <!-- SOCIALS ROW -->
-          <v-col cols="12" class="flex-grow-1">
-            <v-card class="px-5 py-5 h-full">
-              <h3 class="font-semibold text-lg">🔗 Socials</h3>
-              <div v-for="(link, platform) in user.socials" :key="platform">
-                <a :href="link" target="_blank" class="hover:underline">
-                  {{ platform }}: {{ link }}
-                </a>
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
+          <v-card class="px-5 py-5 flex-grow">
+            <h3 class="font-semibold text-lg">🔗 Socials</h3>
+            <div v-for="(link, platform) in user.socials" :key="platform">
+              <a :href="link" target="_blank" class="hover:underline">
+                {{ platform }}: {{ link }}
+              </a>
+            </div>
+          </v-card>
+        </div>
       </v-col>
 
       <!-- Content -->
-      <v-col>
-        <v-row class="h-full gap-6" dense no-gutters>
-          <v-col cols="12">
-            <v-card class="h-full">test</v-card>
-          </v-col>
+      <v-col class="md:h-full">
+        <div class="h-full flex flex-col gap-6">
+          <v-card class="flex-grow">test</v-card>
 
-          <v-col cols="12">
-            <v-card class="h-full">
-              <h2>Projects</h2>
-            </v-card>
-          </v-col>
-        </v-row>
+          <v-card class="flex-grow">
+            <h2>Projects</h2>
+          </v-card>
+        </div>
       </v-col>
     </v-row>
   </v-container>
